8d0c81f4c08b7b278542fe790e29c37cdf3ffb4e
Georges Dubus <georges.dubus@compiletoi.net>
2011-04-15 18:31:05 +0200

    Updated main for testing.

--- re_eat/main.py
+++ re_eat/main.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import sip
+from re_eat.tests.test_recipes import load_some_tags
+
 sip.setapi('QString', 2)
 
 import sys
@@ -9,17 +11,27 @@ from PyQt4.QtGui import *
 
 from re_eat.models import initialize_testing_sql, Session, Tag, Recipe
 from re_eat.tags import TagsWidget
+from re_eat.recipes import RecipesWidget
 
 def main():
     initialize_testing_sql()
 
-    Session.add(Recipe(u"Carbo"))
-    Session.add_all(Tag(t) for t in [u'lourd', u'pâtes', u'hiver'])
-
+    load_some_tags()
 
     app = QApplication(sys.argv)
 
-    w = TagsWidget()
+    l = QVBoxLayout()
+    w = QWidget()
+    w.setLayout(l)
+
+    rw = RecipesWidget()
+    tw = TagsWidget()
+
+    tw.tagsChanged.connect(rw.reload)
+
+    l.addWidget(rw)
+    l.addWidget(tw)
+
     w.show()
 
     app.exec_()
